e07914d7b06593344a27df0c6996030dc1917bc7
Oscar Puentes <feliipepuentes@gmail.com>
2023-08-08 10:52:59 -0500

    adding test cases

--- endpoints/users/POSTCreateUser_test.go
+++ endpoints/users/POSTCreateUser_test.go
@@ -3,8 +3,10 @@ package users
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"gin-microservice/controller/usersController/mocks"
 	usersmodels "gin-microservice/models/usersModels"
+	"gin-microservice/utils"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -40,13 +42,7 @@ func (t *POSTCreateUserTestSuite) Test_CreateUser_Success(){
 		LastName: "Puentes",
 	}
 
-	writer := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(writer)
-
-	bodyData, _ := json.Marshal(userData)
-	bytesBuffer := bytes.NewBuffer(bodyData)
-	request := httptest.NewRequest(http.MethodPost, "/users", bytesBuffer)
-	c.Request = request
+	writer, c := createWriterWithUserPostBody(userData)
 
 	var timeNow time.Time
 	expectedResponse := usersmodels.UserObject{
@@ -70,4 +66,52 @@ func (t *POSTCreateUserTestSuite) Test_CreateUser_Success(){
 	t.Nil(err)
 	t.Equal(http.StatusCreated, writer.Code)
 	t.Equal(expectedResponse, response)
+}
+
+func (t *POSTCreateUserTestSuite) Test_CreateUser_NoValidEntry_Fail(){
+	inputObject := make(map[string]string)
+	inputObject["Data"] = "Value"
+
+	writer, c := createWriterWithUserPostBody(inputObject)
+
+	t.endpoint.CreateUser(c)
+
+	t.Equal(http.StatusBadRequest, writer.Code)
+	t.Equal(utils.ErrInvalidUserObject.Error(), writer.Body.String())
+	t.contoller.AssertNumberOfCalls(t.T(), "CreateUser", 0)
+}
+
+func (t *POSTCreateUserTestSuite) Test_CreateUser_ControllerError_Fail(){
+	userData := usersmodels.User{
+		Username: "Felipe",
+		Password: "12345",
+		Email: "aw@gmail.com",
+		FirstName: "Felipe",
+		LastName: "Puentes",
+	}
+
+	writer, c := createWriterWithUserPostBody(userData)
+
+	expectedError := errors.New("expected error")
+	t.contoller.On("CreateUser", userData).Return(usersmodels.UserObject{}, expectedError)
+
+	t.endpoint.CreateUser(c)
+
+	wrappedError := utils.StringWrapError(utils.ErrUnableToCreateUser, expectedError)
+
+	t.Equal(http.StatusBadRequest, writer.Code)
+	t.Equal(wrappedError, writer.Body.String())
+	t.contoller.AssertNumberOfCalls(t.T(), "CreateUser", 1)
+}
+
+func createWriterWithUserPostBody(input any)(*httptest.ResponseRecorder, *gin.Context){
+	writer := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(writer)
+
+	bodyData, _ := json.Marshal(input)
+	bytesBuffer := bytes.NewBuffer(bodyData)
+	request := httptest.NewRequest(http.MethodPost, "/users", bytesBuffer)
+	c.Request = request
+
+	return writer, c
 }
